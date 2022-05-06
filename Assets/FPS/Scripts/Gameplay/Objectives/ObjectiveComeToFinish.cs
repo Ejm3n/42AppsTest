c667751909a9de4c7da57cb5e41dd702e9ebbe70
Ejm3n <yashaydakov@mail.ru>
2022-05-06 16:41:32 +0300

    firstCommit

---
+++ Assets/FPS/Scripts/Gameplay/Objectives/ObjectiveComeToFinish.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.FPS.Game;
+
+namespace Unity.FPS.Gameplay
+{
+    [RequireComponent(typeof(Collider))]
+    public class ObjectiveComeToFinish : Objective
+    {
+       
+    }
+}
+
